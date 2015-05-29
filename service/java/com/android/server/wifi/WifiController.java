8199f924d747ec1c90a9090ae332b00649f87b9e
Zdrowy Gosciu <ZdrowyGosciu+GITHUB@gmail.com>
2015-05-29 06:59:56 +0200

    Revert "WifiController: fix high perf lock sometimes not respected"
    Possible fix for WiFi HAL making random reboots. This reverts commit c608a6f968d415478668baf8a9d743ad57c0f845.

--- service/java/com/android/server/wifi/WifiController.java
+++ service/java/com/android/server/wifi/WifiController.java
@@ -123,7 +123,6 @@ class WifiController extends StateMachine {
     private StaDisabledWithScanState mStaDisabledWithScanState = new StaDisabledWithScanState();
     private ApEnabledState mApEnabledState = new ApEnabledState();
     private DeviceActiveState mDeviceActiveState = new DeviceActiveState();
-    private DeviceActiveHighPerfState mDeviceActiveHighPerfState = new DeviceActiveHighPerfState();
     private DeviceInactiveState mDeviceInactiveState = new DeviceInactiveState();
     private ScanOnlyLockHeldState mScanOnlyLockHeldState = new ScanOnlyLockHeldState();
     private FullLockHeldState mFullLockHeldState = new FullLockHeldState();
@@ -146,7 +145,6 @@ class WifiController extends StateMachine {
             addState(mApStaDisabledState, mDefaultState);
             addState(mStaEnabledState, mDefaultState);
                 addState(mDeviceActiveState, mStaEnabledState);
-                    addState(mDeviceActiveHighPerfState, mDeviceActiveState);
                 addState(mDeviceInactiveState, mStaEnabledState);
                     addState(mScanOnlyLockHeldState, mDeviceInactiveState);
                     addState(mFullLockHeldState, mDeviceInactiveState);
@@ -418,7 +416,7 @@ class WifiController extends StateMachine {
                             break;
                         }
                         if (mDeviceIdle == false) {
-                            checkLocksAndTransitionWhenDeviceActive();
+                            transitionTo(mDeviceActiveState);
                         } else {
                             checkLocksAndTransitionWhenDeviceIdle();
                         }
@@ -547,7 +545,7 @@ class WifiController extends StateMachine {
                             break;
                         }
                         if (mDeviceIdle == false) {
-                            checkLocksAndTransitionWhenDeviceActive();
+                            transitionTo(mDeviceActiveState);
                         } else {
                             checkLocksAndTransitionWhenDeviceIdle();
                         }
@@ -637,7 +635,7 @@ class WifiController extends StateMachine {
             if (msg.what == CMD_EMERGENCY_MODE_CHANGED && msg.arg1 == 0) {
                 if (mSettingsStore.isWifiToggleEnabled()) {
                     if (mDeviceIdle == false) {
-                        checkLocksAndTransitionWhenDeviceActive();
+                        transitionTo(mDeviceActiveState);
                     } else {
                         checkLocksAndTransitionWhenDeviceIdle();
                     }
@@ -667,9 +665,6 @@ class WifiController extends StateMachine {
             if (msg.what == CMD_DEVICE_IDLE) {
                 checkLocksAndTransitionWhenDeviceIdle();
                 // We let default state handle the rest of work
-            } else if (msg.what == CMD_LOCKS_CHANGED) {
-                checkLocksAndTransitionWhenDeviceActive();
-                return HANDLED;
             } else if (msg.what == CMD_USER_PRESENT) {
                 // TLS networks can't connect until user unlocks keystore. KeyStore
                 // unlocks when the user punches PIN after the reboot. So use this
@@ -684,16 +679,6 @@ class WifiController extends StateMachine {
         }
     }
 
-    /* Parent: DeviceActiveState. Device is active, and an app is holding a high perf lock. */
-    class DeviceActiveHighPerfState extends State {
-        @Override
-        public void enter() {
-            mWifiStateMachine.setOperationalMode(WifiStateMachine.CONNECT_MODE);
-            mWifiStateMachine.setDriverStart(true);
-            mWifiStateMachine.setHighPerfModeEnabled(true);
-        }
-    }
-
     /* Parent: StaEnabledState */
     class DeviceInactiveState extends State {
         @Override
@@ -704,7 +689,7 @@ class WifiController extends StateMachine {
                     updateBatteryWorkSource();
                     return HANDLED;
                 case CMD_SCREEN_ON:
-                    checkLocksAndTransitionWhenDeviceActive();
+                    transitionTo(mDeviceActiveState);
                     // More work in default state
                     return NOT_HANDLED;
                 default:
@@ -747,17 +732,6 @@ class WifiController extends StateMachine {
         @Override
         public void enter() {
             mWifiStateMachine.setDriverStart(false);
-        }
-    }
-
-    private void checkLocksAndTransitionWhenDeviceActive() {
-        if (mLocks.hasLocks() && mLocks.getStrongestLockMode() == WIFI_MODE_FULL_HIGH_PERF) {
-            // It is possible for the screen to be off while the device is
-            // is active (mIdleMillis), so we need the high-perf mode
-            // otherwise powersaving mode will be turned on.
-            transitionTo(mDeviceActiveHighPerfState);
-        } else {
-            transitionTo(mDeviceActiveState);
         }
     }
 
